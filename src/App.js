780b1e74cc0b9ccc1eaba7ee3194f9960234621f
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:23:43 -0400

    Create Footer functional component
    Presentational components
    
    Since each FilterLink needs to know the visibilityFilter, we will make
    that a prop.
    
    Replace FilterLink’s store.dispatch with an onClick call that will take
    a single parameter with the filter.
    
    Since onClick is now a prop for FilterLink, we need to specify it every
    time that FilterLink is used in our Footer. Adding
    onClick={onFilterClick} makes sure that onClick makes it to FilterLink
    as a prop.

--- src/App.js
+++ src/App.js
@@ -64,25 +64,54 @@ const store = createStore(todoApp);
 const FilterLink = ({
   filter,
   currentFilter,
-  children
+  children,
+  onClick
 }) => {
   if (filter === currentFilter) {
     return <span>{children}</span>
   }
   return (
     <a href='#'
-       onClick={e => {
-         e.preventDefault();
-         store.dispatch({
-           type: 'SET_VISIBILITY_FILTER',
-           filter
-         });
-       }}
+      onClick={e => {
+        e.preventDefault();
+        onClick(filter);
+      }}
     >
       {children}
     </a>
-  )
-}
+  );
+};
+
+const Footer = ({
+  visibilityFilter,
+  onFilterClick
+}) => (
+  <p>
+    <FilterLink
+      filter='SHOW_ALL'
+      currentFilter={visibilityFilter}
+      onClick={onFilterClick}
+    >
+      All
+    </FilterLink>
+    {', '}
+    <FilterLink
+      filter='SHOW_ACTIVE'
+      currentFilter={visibilityFilter}
+      onClick={onFilterClick}
+    >
+      Active
+    </FilterLink>
+      {', '}
+    <FilterLink
+      filter='SHOW_COMPLETED'
+      currentFilter={visibilityFilter}
+      onClick={onFilterClick}
+    >
+      Completed
+    </FilterLink>
+  </p>
+);
 
 const getVisibleTodos = (
   todos,
