48c5021c185296b5dfd81d40be165f39fcd75046
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:09:36 -0400

    Extract TodoList to be a presentational component
    The TodoList component will accept an array of todos, and will render
    them into a <ul> by using the todos.map() function to render a Todo
    component for each todo item.
    
    We tell React to use each todo's id as the unique key for the elements,
    and we'll use the spread operator to send the todo object's text and
    completed properties are sent as props to the Todo component.
    
    We need to specify what happens when a Todo is clicked. Since we want
    to keep this as a presentational component, instead of dispatching an
    action, we'll specify a function onTodoClick() and pass it todo.id so
    it can decide what needs to happen. We will also pass onTodoClick as a
    prop to the Todo component.

--- src/App.js
+++ src/App.js
@@ -122,6 +122,20 @@ const Todo = ({
   </li>
 );
 
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
 
 let nextTodoId = 0;
 class TodoApp extends Component {
