fe48923b64a17e7a962a670f42df597262e01080
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:24:54 -0400

    Add Footer to TodoApp
    When adding the Footer component into TodoApp, we need to pass two
    props. First, visibilityFilter to highlight the active link. The second
    prop is onFilterClick, which will dispatch an action of type
    'SET_VISIBILITY_FILTER' along with the filter being clicked.

--- src/App.js
+++ src/App.js
@@ -217,30 +217,15 @@ class TodoApp extends Component {
             })
           }
         />
-        <p>
-          Show:
-          {' '}
-          <FilterLink
-            filter='SHOW_ALL'
-            currentFilter={visibilityFilter}
-          >
-            All
-          </FilterLink>
-          {' '}
-          <FilterLink
-            filter='SHOW_ACTIVE'
-            currentFilter={visibilityFilter}
-          >
-            Active
-          </FilterLink>
-          {' '}
-          <FilterLink
-            filter='SHOW_COMPLETED'
-            currentFilter={visibilityFilter}
-          >
-            Completed
-          </FilterLink>
-        </p>
+        <Footer
+          visibilityFilter={visibilityFilter}
+          onFilterClick={filter =>
+            store.dispatch({
+              type: 'SET_VISIBILITY_FILTER',
+              filter
+            })
+          }
+        />
       </div>
     )
   };
