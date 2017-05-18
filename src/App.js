5ed257a30d2da27712baec767f1230d17535fc1d
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:14:09 -0400

    Use presentational components within TodoApp (container component)
    The TodoApp component renders a TodoList and passes it a function that
    can dispatch an action.
    
    The TodoList component renders the Todo component, and passes an
    onClick prop which calls onTodoClick().
    
    The Todo component uses the onClick prop it receives and binds it to
    the list item's onClick. This way when it's called, the onTodoClick()
    is called, which in turn dispatches the action, which in turn updates
    the visibile todos, since the action updates the store.

--- src/App.js
+++ src/App.js
@@ -163,25 +163,15 @@ class TodoApp extends Component {
           }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={todo.id}
-                onClick={() => {
-                  store.dispatch({
-                    type: 'TOGGLE_TODO',
-                    id: todo.id
-                  });
-                }}
-                style={{
-                  textDecoration:
-                    todo.completed ?
-                      'line-through' :
-                      'none'
-                }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          }
+        />
         <p>
           Show:
           {' '}
