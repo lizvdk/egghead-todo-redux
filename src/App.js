80d930a40d35873ae42774648ab095d389065d99
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:00:37 -0400

    Extract todos and visibilityFilter from the props
    Use ES6 destructuring inside of the TodoApp component.
    
    Now we can access them directly instead of having to type "this.props."
    every time.

--- src/App.js
+++ src/App.js
@@ -104,9 +104,13 @@ const getVisibleTodos = (
 let nextTodoId = 0;
 class TodoApp extends Component {
   render() {
+    const {
+      todos,
+      visibilityFilter
+    } = this.props;
     const visibleTodos = getVisibleTodos(
-      this.props.todos,
-      this.props.visibilityFilter
+      todos,
+      visibilityFilter
     );
     return (
       <div>
