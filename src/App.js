27d8035a451eb883fc28fc55044a335dd789cdc6
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 15:52:24 -0400

    Create a getVisibleTodos()
    getVisibleTodos() will take two arguments: The list of todos and the
    filter. Inside will be a switch statement that operates based on the
    current filter value.

--- src/App.js
+++ src/App.js
@@ -80,6 +80,25 @@ const FilterLink = ({
   )
 }
 
+const getVisibleTodos = (
+  todos,
+  filter
+) => {
+  switch (filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(
+        t => t.completed
+      );
+    case 'SHOW_ACTIVE':
+      return todos.filter(
+        t => !t.completed
+      );
+  }
+}
+
+
 let nextTodoId = 0;
 class TodoApp extends Component {
   render() {
