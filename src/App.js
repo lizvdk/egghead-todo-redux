e8f35f2a9d91734e0171523033b318f9db900aeb
Liz Vanderkloot <liz@lizvdk.com>
2017-05-18 16:15:43 -0400

    Extract the Input and the Button into AddTodo
    Functional components don't have instances, so instead of using this,
    we will use a variable called input that we will close over so we can
    write to it inside of the function.
    
    Since we want AddTodo to be a presentational component, we will have
    the button call an onAddClick() function with input's value as its
    parameter. We also make onAddClick a prop so that the component that
    uses AddTodo can specify what happens with the "Add Todo" button is
    clicked.

--- src/App.js
+++ src/App.js
@@ -104,6 +104,26 @@ const getVisibleTodos = (
   }
 }
 
+const AddTodo = ({
+  onAddClick
+}) => {
+  let input;
+
+  return (
+    <div>
+      <input ref={node => {
+        input = node;
+      }} />
+      <button onClick={() => {
+        onAddClick(input.value);
+        input.value = '';
+      }}>
+        Add Todo
+      </button>
+    </div>
+  );
+};
+
 const Todo = ({
   onClick,
   completed,
