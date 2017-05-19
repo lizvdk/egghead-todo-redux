0052164d0e275fb443a82b5795438280279d585d
Liz Vanderkloot <liz@lizvdk.com>
2017-05-19 09:17:48 -0400

    Create VisibleTodoList container component
    So that the TodoList component can remain purely presentational, we are
    encapsulate reading the currently visible todos into a container
    component (VisableTodoList).
    
    Data for the VisibleTodoList will be calculated by using the current
    state.
    
    We use getVisibleTodos() to determine which todos to show based on the
    visibilityFilter.
    
    The VisibleTodoList container component also specifies the behavior of
    onTodoClick() and dispatch the ‘TOGGLE_TODO’ action.
    
    We force re-render the VisibleTodoList just like the FilterLink
    component because If the parent component (VisibleTodoList) doesn’t
    update the
    store when the store is updated, the correct todos won’t be rendered.
    The component must subscribe to the store.
    
    *Container components such as FilterLink and connect a presentational
    component to the Redux store, and specify the data and behavior that it
    needs.

--- src/App.js
+++ src/App.js
@@ -198,6 +198,40 @@ const Todo = ({
   </li>
 );
 
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() =>
+      this.forceUpdate()
+    );
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  render() {
+    const props = this.props;
+    const state = store.getState();
+
+    return (
+      <TodoList
+        todos={
+          getVisibleTodos(
+            state.todos,
+            state.visibilityFilter
+          )
+        }
+        onTodoClick={id =>
+          store.dispatch({
+            type: 'TOGGLE_TODO',
+            id
+          })
+        }
+      />
+    );
+  }
+}
+
 const TodoList = ({
   todos,
   onTodoClick
