fe1ca7118e71f583099324acfeac1523a369f52c
Liz Vanderkloot <liz@lizvdk.com>
2017-05-19 10:22:40 -0400

    Update Provider component to use React's context feature
    This is what makes the store available to any component that it
    renders. Now the store will be available to any of the children and
    grandchildren of the components Provider renders.
    
    e have to provide childContextTypes on the component that provides
    child context. This is similar to React's PropTypes definition that
    helps you when writing your app, except that in this case it is
    required in order for the child components to receive context.

--- src/App.js
+++ src/App.js
@@ -60,9 +60,18 @@ const todoApp = combineReducers({
 });
 
 class Provider extends Component {
+  getChildContext() {
+    return {
+      store: this.props.store // This corresponds to the `store` passed in as a prop
+    };
+  }
   render() {
     return this.props.children;
   }
+}
+
+Provider.childContextTypes = {
+  store: React.PropTypes.object
 }
 
 const Link = ({
