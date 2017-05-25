70739944077725adf06a74e1886729da1f1b76cd
Liz Vanderkloot <liz@lizvdk.com>
2017-05-25 14:42:23 -0400

    Generate FooterLink
    * mapStateToProps accepts the state of the Redux store, and returns the
    props that should be passed to the Link component.
    * It's common to use the container props when calculating the child
    props, so we pass them in as a second argument to mapStateToProps. In
    this case, we'll rename it to ownProps to make it more clear that we
    are talking about the container component's own props, and not the
    props that are passed to the child, which is the return value of
    mapStateToProps.
    * mapDispatchToProps: In this case, we only have the onClick() where we
    dispatch the action of type 'SET_VISIBILITY_FILTER' along with the
    filter type. Since there is another reference to props, we will add
    ownProps as our second argument to mapDispatchToLinkProps.

--- src/App.js
+++ src/App.js
@@ -81,42 +81,29 @@ const Link = ({
   );
 };
 
-class FilterLink extends Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
+const mapStateToLinkProps = (state, ownProps) => {
+  return {
+    active:
+      ownProps.filter === state.visibilityFilter
   }
-  componentWillUnmount() {
-    this.unsubscribe(); // return value of `store.subscribe()`
-  }
-  render () {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={
-          props.filter ===
-          state.visibilityFilter
-        }
-        onClick={() =>
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }
-      >
-        {props.children}
-      </Link>
-    );
-  }
+};
+const mapDispatchToLinkProps = (
+  dispatch,
+  ownProps
+) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      });
+    }
+  };
 }
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
-}
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
 
 const Footer = ({ store }) => (
   <p>
